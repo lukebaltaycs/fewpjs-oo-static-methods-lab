2cbda2f1b2359c34b64d92977ab41d9bf2ac4a43
Luke Baltay <luke.baltay@yale.edu>
2022-07-22 21:25:59 -0400

    commit tree 2d53544754ee63c9776eafceb0606e72fc72fb83

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  }
+
+  static sanitize(word) {
+    return word.replace( /[^A-Za-z0-9 '-]/g, '' );
+  }
+
+  static titleize(word) {
+    const ignore = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+
+    let answer = [];
+
+    const words = word.split(" ");
+    for (let i = 0; i < words.length; i++){
+      if (i == 0) {
+        words[i] = Formatter.capitalize(words[i]);
+      } else if (!ignore.includes(words[i])){
+        words[i] = Formatter.capitalize(words[i]);
+      }
+    }
+    return words.join(" ");
+  }
 }
